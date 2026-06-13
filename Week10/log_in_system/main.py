35e9479033302d07980c17ff1af16324caceb8c9
Nehe <133280575+Nbrown2309@users.noreply.github.com>
2026-06-14 09:16:21 +1200

    Week10-Act1.2(Improved Code)

--- Week10/log_in_system/main.py
+++ Week10/log_in_system/main.py
@@ -1,7 +1,19 @@
+"""
+Log in and signup System 
+
+This program allows users to:
+- create an account (sign up)
+- log in to their account
+- reset their password if they forget it
+- manage their account information
+"""
+
 from services.auth_service import AuthService
 
+# Create an instance of the AuthService class to handle authentication
 auth = AuthService()
 
+# Start the main loop of the application
 while True:
 
 # Making the UI for the login system
