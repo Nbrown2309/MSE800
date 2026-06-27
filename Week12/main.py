01ec600b20ad9b83ed2fc48b62fd8d51a1a5c7b9
Nehe <133280575+Nbrown2309@users.noreply.github.com>
2026-06-27 12:19:04 +1200

    Week12-Act3.1

--- Week12/main.py
+++ Week12/main.py
@@ -16,9 +16,10 @@ def home():
 def bye():
     return "<h1>Goodbye, Flask!</h1>"
 
-@app.route("/username/<name>")
-def learn(name):
-    return f"<h1>{name} is learning Flask!</h1>"
+
+@app.route("/<name>/<int:number>")
+def learn(name, number):
+    return f"<h1>{name} is learning Flask! She wakes up at {number} o'clock in the morning.</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
